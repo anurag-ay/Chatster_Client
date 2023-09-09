d6786acd1850022ec832d723a97d79362d484e9c
anurag-ay <ay1909069@gmail.com>
2023-09-09 22:17:34 +0530

    add NavBar

--- src/App.js
+++ src/App.js
@@ -10,94 +10,102 @@ import { Call, Chat, NotificationAdd, Search } from "@mui/icons-material";
 import { useState } from "react";
 import ChatSidebar from "./components/ChatSidebar";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   const [chatActive, setChatActive] = useState(true);
   const [callActive, setCallActive] = useState(false);
   const [notificationActive, setNotificationActive] = useState(false);
   return (
-    <Stack direction="row" height="100vh">
-      <Box
-        sx={{
-          backgroundColor: "#fafafa",
-          overflowY: "scroll",
-          minWidth: "28vw",
-        }}
-      >
-        <Box sx={{ padding: "0.5em" }}>
-          <Paper
-            component="form"
-            sx={{
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Search..." />
-            <IconButton type="button" sx={{ p: "0.4em" }} aria-label="search">
-              <Search />
-            </IconButton>
-          </Paper>
-        </Box>
-        <Stack
-          direction="row"
-          alignItems="center"
-          justifyContent="space-evenly"
-          sx={{ p: "1em" }}
+    <>
+      <NavBar />
+      <Stack direction="row" height="90vh">
+        <Box
+          sx={{
+            backgroundColor: "#fafafa",
+            overflowY: "scroll",
+            minWidth: "28vw",
+          }}
         >
-          <IconButton
-            onClick={() => {
-              if (chatActive === false) {
-                setChatActive(true);
-                setCallActive(false);
-                setNotificationActive(false);
-              }
-            }}
+          <Box sx={{ padding: "0.5em" }}>
+            <Paper
+              component="form"
+              sx={{
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Search..." />
+              <IconButton type="button" sx={{ p: "0.4em" }} aria-label="search">
+                <Search />
+              </IconButton>
+            </Paper>
+          </Box>
+          <Stack
+            direction="row"
+            alignItems="center"
+            justifyContent="space-evenly"
+            sx={{ p: "1em" }}
           >
-            {chatActive ? (
-              <Chat sx={{ color: "black", height: "1.2em", width: "1.2em" }} />
-            ) : (
-              <Chat />
-            )}
-          </IconButton>
-          <IconButton
-            onClick={() => {
-              if (callActive === false) {
-                setCallActive(true);
-                setChatActive(false);
-                setNotificationActive(false);
-              }
-            }}
-          >
-            {callActive ? (
-              <Call sx={{ color: "black", height: "1.2em", width: "1.2em" }} />
-            ) : (
-              <Call />
-            )}
-          </IconButton>
-          <IconButton
-            onClick={() => {
-              if (notificationActive === false) {
-                setNotificationActive(true);
-                setChatActive(false);
-                setCallActive(false);
-              }
-            }}
-          >
-            {notificationActive ? (
-              <NotificationAdd
-                sx={{ color: "black", height: "1.2em", width: "1.2em" }}
-              />
-            ) : (
-              <NotificationAdd />
-            )}
-          </IconButton>
-        </Stack>
-        <Divider orientation="horizontal" />
+            <IconButton
+              onClick={() => {
+                if (chatActive === false) {
+                  setChatActive(true);
+                  setCallActive(false);
+                  setNotificationActive(false);
+                }
+              }}
+            >
+              {chatActive ? (
+                <Chat
+                  sx={{ color: "black", height: "1.2em", width: "1.2em" }}
+                />
+              ) : (
+                <Chat />
+              )}
+            </IconButton>
+            <IconButton
+              onClick={() => {
+                if (callActive === false) {
+                  setCallActive(true);
+                  setChatActive(false);
+                  setNotificationActive(false);
+                }
+              }}
+            >
+              {callActive ? (
+                <Call
+                  sx={{ color: "black", height: "1.2em", width: "1.2em" }}
+                />
+              ) : (
+                <Call />
+              )}
+            </IconButton>
+            <IconButton
+              onClick={() => {
+                if (notificationActive === false) {
+                  setNotificationActive(true);
+                  setChatActive(false);
+                  setCallActive(false);
+                }
+              }}
+            >
+              {notificationActive ? (
+                <NotificationAdd
+                  sx={{ color: "black", height: "1.2em", width: "1.2em" }}
+                />
+              ) : (
+                <NotificationAdd />
+              )}
+            </IconButton>
+          </Stack>
+          <Divider orientation="horizontal" />
 
-        {chatActive ? <ChatSidebar /> : null}
-      </Box>
-      <Box flex="2">Sidebar</Box>
-    </Stack>
+          {chatActive ? <ChatSidebar /> : null}
+        </Box>
+        <Box flex="2">Sidebar</Box>
+      </Stack>
+    </>
   );
 }
 
