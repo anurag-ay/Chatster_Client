715bd872438f948c3bdc9d429966c2cf94dfcfb2
anurag-ay <ay1909069@gmail.com>
2023-09-23 17:23:09 +0530

    Move context consumption from index.js to app.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import UserInfoProvider from "./context/userInfoContex";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <UserInfoProvider>
-    <App />
-  </UserInfoProvider>
-);
+root.render(<App />);
