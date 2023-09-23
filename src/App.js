d9525ead98aa17f647d99c5bcdd12089132e04d4
Mohd_Shahwaz <mohdshahwaz9305@gmail.com>
2023-09-23 19:30:45 +0530

    add login and signup

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import "./App.css";
 import ChatApp from "./pages/ChatApp";
 import UserInfoProvider from "./context/userInfoContex";
+import LogIn from "./components/LogIn";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
     <UserInfoProvider>
-      <ChatApp />
+      {/* <ChatApp /> */}
+      {/* <LogIn/> */}
+      <SignUp/>
     </UserInfoProvider>
 
   );
