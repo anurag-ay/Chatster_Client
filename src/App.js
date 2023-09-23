7aa88b9699a8131fc545e30c64285490603e64b5
anurag-ay <ay1909069@gmail.com>
2023-09-23 17:22:20 +0530

    Modify app.js to make components one level up

--- src/App.js
+++ src/App.js
@@ -1,49 +1,13 @@
-import { Box, Stack } from "@mui/material";
-import ChatSidebar from "./components/ChatSidebar";
+import React from "react";
 import "./App.css";
-import NavBar from "./components/NavBar";
-import SidebarItems from "./components/SidebarItems";
-import { useEffect, useState } from "react";
-import CallSidebar from "./components/CallSidebar";
-import Chatbody from "./components/Chatbody";
-// import CallUi from "./components/CallUi";
-import { io } from "socket.io-client";
+import ChatApp from "./pages/ChatApp";
+import UserInfoProvider from "./context/userInfoContex";
 
 function App() {
-  const [chatActive, setChatActive] = useState(true);
-  const [callActive, setCallActive] = useState(false);
-  const [notificationActive, setNotificationActive] = useState(false);
-
-  useEffect(() => {
-    io("http://localhost:5000");
-  }, []);
-
   return (
-    <Box>
-      {/* <CallUi /> */}
-      <NavBar />
-      <Stack direction="row" mt="4em">
-        <Box
-          sx={{
-            backgroundColor: "#fafafa",
-          }}
-        >
-          <SidebarItems
-            chatActive={chatActive}
-            setChatActive={setChatActive}
-            callActive={callActive}
-            setCallActive={setCallActive}
-            notificationActive={notificationActive}
-            setNotificationActive={setNotificationActive}
-          />
-          {chatActive ? <ChatSidebar /> : null}
-          {callActive ? <CallSidebar /> : null}
-        </Box>
-        <Box flex="2" maxWidth={"73.6vw"}>
-          <Chatbody />
-        </Box>
-      </Stack>
-    </Box>
+    <UserInfoProvider>
+      <ChatApp />
+    </UserInfoProvider>
   );
 }
 
