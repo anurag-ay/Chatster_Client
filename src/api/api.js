002a20c49b12ff4f2f1fb1555a1a6e8aca1e8789
anurag-ay <ay1909069@gmail.com>
2023-09-23 12:14:04 +0530

    Add api routes

---
+++ src/api/api.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export const BASE_URL = "http://localhost:5000";
+
+// Intilializing Axios with Base Url
+export default axios.create({
+  baseURL: BASE_URL,
+});
+
+// User Routes
+export const registerUserRoute = "/api/user/register";
+export const logInRoute = "/api/user/login";
+export const getUserRoute = "api/user/getUser";
+export const addContactRoute = "api/user/addContact";
+export const getContacts = "api/user/getContacts";
+
+// Chat Route
+export const chatRoute = "api/chats";
