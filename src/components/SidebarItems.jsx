5961b4953db81a9ebd9131c1d041387243676182
anurag-ay <ay1909069@gmail.com>
2023-09-26 23:03:40 +0530

    Feat search contact

--- src/components/SidebarItems.jsx
+++ src/components/SidebarItems.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Call, Chat, NotificationAdd, Search } from "@mui/icons-material";
+import axios, { searchUserRoute } from "../api/api";
 
 import {
   Box,
@@ -9,9 +10,15 @@ import {
   IconButton,
   Divider,
 } from "@mui/material";
+import { useUserInfo } from "../context/userInfoContex";
 
 export default function SidebarItems(props) {
+  const userInfo = useUserInfo();
+
   const {
+    searchText,
+    setSearchText,
+    setsearchedUserArray,
     chatActive,
     setChatActive,
     callActive,
@@ -20,19 +27,38 @@ export default function SidebarItems(props) {
     setNotificationActive,
   } = props;
 
+  useEffect(() => {
+    if (userInfo) {
+      const searchUsers = async () => {
+        if (searchText) {
+          const res = await axios.get(
+            `${searchUserRoute}/${searchText}/${userInfo?.userName}`
+          );
+          const users = res.data;
+          setsearchedUserArray(users);
+        }
+      };
+      searchUsers();
+    }
+  }, [searchText, setsearchedUserArray, userInfo]);
+
   return (
     <Box>
       {/* Search */}
-      <Box sx={{ padding: "0.5em" }}>
+      <Box component="form" sx={{ padding: "0.5em" }}>
         <Paper
-          component="form"
           sx={{
             display: "flex",
             alignItems: "center",
           }}
         >
-          <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Search..." />
-          <IconButton type="button" sx={{ p: "0.4em" }} aria-label="search">
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="Search Contacts by User Name"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <IconButton type="submit" sx={{ p: "0.4em" }} aria-label="search">
             <Search />
           </IconButton>
         </Paper>
