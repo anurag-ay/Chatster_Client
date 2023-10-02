071d0e907e87897c855c8969164f1484549bbd91
anurag-ay <ay1909069@gmail.com>
2023-10-02 14:45:06 +0530

    Modify format Time method

--- src/utils/formatTimeStamp.js
+++ src/utils/formatTimeStamp.js
@@ -1,6 +1,7 @@
 export default function formatTime(IsoTime) {
   let timestamp = IsoTime;
   let date = new Date(timestamp);
+  let currentDate = new Date();
 
   let hours = date.getHours();
   let minutes = date.getMinutes();
@@ -12,5 +13,27 @@ export default function formatTime(IsoTime) {
 
   let strTime = hours + ":" + minutes + " " + ampm;
 
-  return strTime;
+  // check if the date is the same as the current date
+  if (date.toDateString() === currentDate.toDateString()) {
+    // check if the time difference is less than a minute
+    let diff = currentDate.getTime() - date.getTime();
+    if (diff < 60000) {
+      return "recently";
+    } else {
+      return strTime;
+    }
+  } else {
+    // check if the date is yesterday
+    let yesterday = new Date(currentDate.getTime() - 86400000);
+    if (date.toDateString() === yesterday.toDateString()) {
+      return "Yesterday " + strTime;
+    } else {
+      // format the date as day/fullMonth/year
+      let day = date.getDate();
+      let month = date.toLocaleString("default", { month: "long" });
+      let year = date.getFullYear();
+      let strDate = day + "/" + month + "/" + year;
+      return strTime + " " + strDate;
+    }
+  }
 }
