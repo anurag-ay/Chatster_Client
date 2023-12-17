83b3a0f3ab18d3cb407b5f8a7d6a556320f6768a
anurag-ay <ay1909069@gmail.com>
2023-12-17 20:50:38 +0530

    fix typo

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const BASE_URL = "http://localhost:5000";
 
-// Intilializing Axios with Base Url
+// Initializing Axios with Base Url
 export default axios.create({
   baseURL: BASE_URL,
 });
